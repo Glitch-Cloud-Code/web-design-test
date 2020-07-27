53fe232b13184c031e54f59cf00374ac3eb13007
Glitch-Cloud <sergey.smirnov.work@gmail.com>
2020-07-27 16:36:47 +0300

    Implemented deletion and editing logic for table datasource

--- src/app/components/records-table/records-table.component.ts
+++ src/app/components/records-table/records-table.component.ts
@@ -1,21 +1,21 @@
-import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
-import { RecordsService } from 'src/app/services/records.service';
-import { RecordsDataSource } from 'src/app/data-sources/records-datasource';
-import { MatSort } from '@angular/material/sort';
+import { Component, OnInit, AfterViewInit, ViewChild } from "@angular/core";
+import { RecordsService } from "src/app/services/records.service";
+import { RecordsDataSource } from "src/app/data-sources/records-datasource";
+import { Record } from "../../interfaces/record"
+import { MatSort } from "@angular/material/sort";
 
 @Component({
-  selector: 'app-records-table',
-  templateUrl: './records-table.component.html',
-  styleUrls: ['./records-table.component.scss']
+  selector: "app-records-table",
+  templateUrl: "./records-table.component.html",
+  styleUrls: ["./records-table.component.scss"],
 })
 export class RecordsTableComponent implements OnInit, AfterViewInit {
-
   public dataSource: RecordsDataSource;
-  public displayedColumns = ['id', 'name', 'surname', 'send_mail_at', 'hash'];
+  public displayedColumns = ["id", "name", "surname", "send_mail_at", "hash", "actions"];
 
   @ViewChild(MatSort) sort: MatSort;
 
-  constructor(private recordsService: RecordsService) { }
+  constructor(private recordsService: RecordsService) {}
 
   ngOnInit(): void {
     this.dataSource = new RecordsDataSource(this.recordsService);
@@ -23,10 +23,23 @@ export class RecordsTableComponent implements OnInit, AfterViewInit {
   }
 
   ngAfterViewInit() {
+    this.sort.sortChange.subscribe(() =>
+      this.dataSource.getAllRecords(
+        this.sort.direction.toUpperCase(),
+        this.sort.active
+      )
+    );
+  }
 
-    // reset the paginator after sorting
-    this.sort.sortChange.subscribe(() => this.dataSource.getAllRecords(this.sort.direction.toUpperCase(), this.sort.active));
+  public addRecord() {
+    this.dataSource.addNewRecord({name: 'Add', surname: "Test", send_mail_at: new Date().toISOString()} as Record);
+  }
 
-}
+  public deleteRecord(recordId: number) {
+    this.dataSource.deleteRecord(recordId);
+  }
 
+  public editRecord(recordId: number) {
+    this.dataSource.editRecord(recordId, {name: 'EditedName', surname: "EditedSurname"} as Record)
+  }
 }
--- src/app/data-sources/records-datasource.ts
+++ src/app/data-sources/records-datasource.ts
@@ -1,36 +1,143 @@
-import { Record } from '../interfaces/record';
-import { BehaviorSubject, of, Observable } from 'rxjs';
-import { catchError, finalize } from 'rxjs/operators'
-import { RecordsService } from '../services/records.service';
-import {CollectionViewer, DataSource} from "@angular/cdk/collections";
+import { Record } from "../interfaces/record";
+import {
+  BehaviorSubject,
+  of,
+  Observable,
+  forkJoin,
+  interval
+} from "rxjs";
+import { catchError, finalize, debounceTime, take} from "rxjs/operators";
+import { RecordsService } from "../services/records.service";
+import { CollectionViewer, DataSource } from "@angular/cdk/collections";
+
 
 export class RecordsDataSource implements DataSource<Record> {
-
-  private lessonsSubject = new BehaviorSubject<Record[]>([]);
+  private recordsSubject = new BehaviorSubject<Record[]>([]);
   private loadingSubject = new BehaviorSubject<boolean>(false);
-
   public loading$ = this.loadingSubject.asObservable();
 
-  constructor(private coursesService: RecordsService) {}
+  constructor(private recordsService: RecordsService) {}
 
   connect(collectionViewer: CollectionViewer): Observable<Record[]> {
-      return this.lessonsSubject.asObservable();
+    return this.recordsSubject.asObservable();
   }
 
   disconnect(collectionViewer: CollectionViewer): void {
-      this.lessonsSubject.complete();
-      this.loadingSubject.complete();
+    this.recordsSubject.complete();
+    this.loadingSubject.complete();
   }
 
-  getAllRecords(sortDirection = 'ASC', sortBy = 'id') {
+  getAllRecords(sortDirection = "ASC", sortBy = "id") {
+    this.loadingSubject.next(true);
 
-      this.loadingSubject.next(true);
-
-      this.coursesService.getAllRecords(sortDirection,
-          sortBy).pipe(
-          catchError(() => of([])),
-          finalize(() => this.loadingSubject.next(false))
+    this.recordsService
+      .getAllRecords(sortDirection, sortBy)
+      .pipe(
+        catchError(() => of([])),
+        finalize(() => this.loadingSubject.next(false))
       )
-      .subscribe(records => this.lessonsSubject.next(records['data']));
+      .subscribe((success) => this.recordsSubject.next(success["data"]));
+  }
+
+  addNewRecord(record: Record) {
+    this.loadingSubject.next(true);
+    this.recordsService
+      .addRecord(record)
+      .pipe(
+        catchError(() => of([])),
+        finalize(() => this.loadingSubject.next(false))
+      )
+      .subscribe((success) => {
+        this.addRecordLocally(success["data"] as Record);
+      });
+  }
+
+  deleteRecord(recordId: number) {
+    this.loadingSubject.next(true);
+    this.recordsService
+      .deleteRecord(recordId)
+      .pipe(
+        catchError(() => of([])),
+        finalize(() => this.loadingSubject.next(false))
+      )
+      .subscribe((success) => {
+        this.deleteRecordLocally(recordId);
+      });
+  }
+
+  editRecord(recordId: number, newRecord: Record) {
+    let editedFields = this.getEditedFields(recordId, newRecord);
+    if (editedFields.length < 1) {
+      return;
+    }
+
+    this.sendPatchRequestsAndUpdateRecordLocally(editedFields, recordId, newRecord);
+  }
+
+  private async sendPatchRequestsAndUpdateRecordLocally(
+    editedFields: String[],
+    recordId: number,
+    newRecord: Record
+  ) {
+    let observables = [];
+    for (let i = 0; i < editedFields.length; i++) {
+      switch (editedFields[i]) {
+        case "name": {
+          observables.push(this.recordsService
+            .patchName(recordId, newRecord.name));
+          break;
+        }
+        case "surname": {
+          observables.push(this.recordsService
+            .patchSurname(recordId, newRecord.surname));
+          break;
+        }
+      }
+    }
+    interval(20).pipe(take(observables.length)).subscribe((x) =>
+      observables[x].subscribe((success)=> {
+        this.replaceRecordLocally(recordId, success['data']);
+      })
+    );
+  }
+
+  private addRecordLocally(record: Record) {
+    this.recordsSubject.value.push(record);
+    this.recordsSubject.next(this.recordsSubject.value);
+  }
+
+  private replaceRecordLocally(id: number, newRecord: Record) {
+    let newValue = this.recordsSubject.value.map((record: Record) => {
+      if (record.id == id) {
+        return newRecord;
+      } else {
+        return record;
+      }
+    });
+
+    this.recordsSubject.next(newValue);
+  }
+
+  private deleteRecordLocally(id: number) {
+    let newValue = this.recordsSubject.value.filter(
+      (record) => record.id != id
+    );
+    this.recordsSubject.next(newValue);
+  }
+
+  private getEditedFields(recordId: number, newRecord: Record): String[] {
+    let result: String[] = [];
+    let currentRecord = this.getLocalRecord(recordId);
+    if (currentRecord.name != newRecord.name) {
+      result.push("name");
+    }
+    if (currentRecord.surname != newRecord.surname) {
+      result.push("surname");
+    }
+    return result;
+  }
+
+  private getLocalRecord(id: number) {
+    return this.recordsSubject.value.find((record) => record.id == id);
   }
 }
